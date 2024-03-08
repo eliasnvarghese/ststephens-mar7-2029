dd9603596c538eb3a5910d1782b8c2b3536b0ea1
Elias Naduvath Varghese <elias.naduvathvarghese@samsclub.com>
2024-03-07 16:59:34 -0800

    first commit

---
+++ public_html/boardoftrustees.php
@@ -0,0 +1,244 @@
+<?php ob_start(); ?>
+<?php session_start() ?>
+<?php
+include("includes/usercredentials.php");
+?>
+<!DOCTYPE html>
+
+<head>
+	<!-- This site is optimized with the Yoast SEO plugin v21.9.1 - https://yoast.com/wordpress/plugins/seo/ -->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Board of Trustees - St. Stephen's Jacobite Syriac Orthodox Church, San Jose (San Francisco Bay Area), CA</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+	<meta name="description" content="Board of Trustees - St. Stephen's Jacobite Syriac Orthodox Church, San Jose (San Francisco Bay Area), CA" />
+	<link rel="canonical" href="https://ststephenssanjose.org/boardoftrustees.php" />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content="Board of Trustees - St. Stephen's Jacobite Syriac Orthodox Church, San Jose (San Francisco Bay Area), CA" />
+	<meta property="og:description" content="Board of Trustees - St. Stephen's Jacobite Syriac Orthodox Church, San Jose (San Francisco Bay Area), CA" />
+	<meta property="og:url" content="https://ststephenssanjose.org/boardoftrustees.php" />
+	<meta property="og:site_name" content="Board of Trustees - St. Stephen's Jacobite Syriac Orthodox Church, San Jose (San Francisco Bay Area), CA" />
+	<meta property="article:publisher" content="https://www.facebook.com/ststephens.church.9" />
+	<meta property="article:modified_time" content="2023-02-26T18:46:51+00:00" />
+	<meta property="og:image" content="https://ststephenssanjose.org/cmsapp/images/bot/president.jpg" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:label1" content="Est. reading time" />
+	<meta name="twitter:data1" content="2 minutes" />
+
+
+	<!-- Google Fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Noto+Serif:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+
+	<!-- Styles -->
+	<link href="font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css" />
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="css/owl-carousel.css" type="text/css" />
+	<link rel="stylesheet" href="css/mediaelementplayer.min.css" />
+	<link href="css/revolution.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="css/settings.css" media="screen" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+	<link href="css/responsive.css" rel="stylesheet" type="text/css" />
+	<link rel="alternate stylesheet" type="text/css" href="css/colors/red.css" title="color1" />
+	<link rel="alternate stylesheet" type="text/css" href="css/colors/wedgewood.css" title="color2" />
+	<link rel="stylesheet" type="text/css" href="css/colors/blue.css" title="color3" />
+	<link rel="alternate stylesheet" type="text/css" href="css/colors/green.css" title="color4" />
+	<link rel="alternate stylesheet" type="text/css" href="css/colors/darkgreen.css" title="color5" />
+
+	<style>
+		.member-detail {
+			position: relative;
+		}
+		.logo img {
+			width: auto;
+		}
+		.one_third,
+		.five_sixth {
+			float: left;
+		}
+		.one_third {
+			width: 33.3%;
+		}
+
+		@media screen and (max-width: 600px) {
+			.one_third,
+			.five_sixth {
+				clear: both;
+				width: 100%;
+				margin-left: 0;
+				margin-right: 0;
+				margin-bottom: 1em;
+			}
+		}
+
+		.one_third {
+			width: 32% !important;
+			margin-right: 2% !important;
+		}
+
+		.one_third.last_column {
+			width: 32% !important;
+			margin-right: 0px !important;
+		}
+
+	</style>
+</head>
+
+<body>
+	<div class="theme-layout">
+		<?php include("includes/pageheader.php"); ?>
+		<div class="page-top">
+			<div class="parallax" style="background:url(images/parallax1.jpg);"></div>
+			<div class="container">
+				<h1><span>BOARD OF TRUSTEES</span></h1>
+				<ul>
+					<li><a href="index.html" title="">Home</a></li>
+				</ul>
+			</div>
+		</div><!--- PAGE TOP -->
+		<section>
+			<div class="block">
+				<div class="container">
+					<div class="row">
+						<div class="title2">
+							<span></span>
+							<h4><span>You can reach out to the Board of Trustees at <a href="mailto: ststephensbayarea@gmail.comm">ststephensbayarea@gmail.com</a></span></h4>
+						</div>
+						<div class="about">
+
+							<img src="cmsapp/images/bot/president.jpg" alt="" width="300" height="300" />
+							<h4 style="font-size: 15px;margin-bottom: 15px;margin-top: 15px;margin-left: 2px;line-height: 24px;">
+								Rev. Fr. Daniel John (Noble Achen)<span style="color:#0d8ce6"> (Vicar &amp; President)</span></h4>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<div class="one_third">
+								<img src="cmsapp/images/bot/vice-president.jpg" alt="" width="300" height="300" />
+								<h4 style="font-size: 15px;margin-bottom: 15px;margin-left: 2px;margin-top: 15px;line-height: 24px;">
+									Elias Naduvath Varghese<span style="color:#0d8ce6"> (Vice President)</span> </h4>
+							</div>
+							<div class="one_third">
+								<img src="cmsapp/images/bot/secretary.jpg" alt="" width="300" height="300" />
+								<h4 style="font-size: 15px;margin-bottom: 15px;margin-top: 15px;margin-left: 50px;line-height: 24px;">
+									George Kurien<span style="color:#0d8ce6"> (Secretary)</span></h4>
+							</div>
+							<div class="one_third last_column">
+								<img src="cmsapp/images/bot/treasurer.jpg" alt="" width="300" height="300" />
+								<h4 style="font-size: 15px;margin-bottom: 15px;margin-top: 15px;margin-left: 50px;line-height: 24px;">
+									Gino John<span style="color:#0d8ce6"> (Treasurer)</span></h4>
+							</div>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<div class="clear_column"></div>
+							<div class="one_third">
+								<img src="cmsapp/images/bot/member1.jpg" alt="" width="300" height="300" />
+								<h4 style="font-size: 15px;margin-bottom: 15px;margin-top: 15px;margin-left: 50px;line-height: 24px;">
+									Aju Varghese<span style="color:#0d8ce6"> (Member)</span></h4>
+							</div>
+							<div class="one_third">
+								<img src="cmsapp/images/bot/member2.jpg" alt="" width="300" height="300" />
+								<h4 style="font-size: 15px;margin-bottom: 15px;margin-top: 15px;margin-left: 50px;line-height: 24px;">
+									Baby Varghese<span style="color:#0d8ce6"> (Member)</span></h4>
+							</div>
+							<div class="one_third last_column">
+								<img src="cmsapp/images/bot/member3.jpg" alt="" width="300" height="300" />
+								<h4 style="font-size: 15px;margin-bottom: 15px;margin-top: 15px;margin-left: 50px;line-height: 24px;">
+									Joel Mathew<span style="color:#0d8ce6"> (Member)</span></h4>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<?php include("includes/pagefooter.php"); ?>
+	</div>
+	<!-- SCRIPTS-->
+	<script type="text/javascript" src="js/modernizr.custom.17475.js"></script>
+	<script src="js/jquery.1.10.2.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.poptrox.min.js" type="text/javascript"></script>
+	<script src="js/enscroll-0.5.2.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/mediaelement-and-player.min.js"></script>
+	<script src="js/script.js"></script>
+	<script src="js/styleswitcher.js"></script>
+	<script src="js/jquery.isotope.min.js"></script>
+	<script src="js/jquery.minimalect.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="js/jquery.downCount.js"></script>
+	<script>
+		$(window).load(function() {
+			$(function() {
+				var $portfolio = $('.mas-gallery');
+				$portfolio.isotope({
+					masonry: {
+						columnWidth: 1
+					}
+				});
+			});
+		});
+	</script>
+	<script>
+		$(document).ready(function() {
+			$("#select1").minimalect({
+				theme: "bubble",
+				placeholder: "Select Gender"
+			});
+			$("#select2").minimalect({
+				theme: "bubble",
+				placeholder: "Select Age"
+			});
+			$("#select3").minimalect({
+				theme: "bubble",
+				placeholder: "Select Your Area"
+			});
+			$(".tweets-slides").owlCarousel({
+				autoPlay: 5000,
+				slideSpeed: 1000,
+				singleItem: true,
+				transitionStyle: "fadeUp",
+				navigation: false
+			}); /*** TWEETS CAROUSEL ***/
+			$(".team-carousel").owlCarousel({
+				autoPlay: 8000,
+				rewindSpeed: 3000,
+				slideSpeed: 2000,
+				items: 4,
+				itemsDesktop: [1199, 3],
+				itemsDesktopSmall: [979, 2],
+				itemsTablet: [768, 2],
+				itemsMobile: [479, 1],
+				navigation: false,
+			}); /*** TEAM CAROUSEL ***/
+		});
+		$('audio,video').mediaelementplayer();
+	</script>
+	<!-- SLIDER REVOLUTION -->
+	<script type="text/javascript" src="js/revolution/jquery.themepunch.tools.min.js"></script>
+	<script type="text/javascript" src="js/revolution/jquery.themepunch.revolution.min.js"></script>
+	<script type="text/javascript" src="js/revolution/extensions/revolution.extension.slideanims.min.js"></script>
+	<script type="text/javascript" src="js/revolution/extensions/revolution.extension.layeranimation.min.js"></script>
+	<script type="text/javascript" src="js/revolution/extensions/revolution.extension.navigation.min.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function() {
+			jQuery("#slider1").revolution({
+				sliderType: "standard",
+				sliderLayout: "fullwidth",
+				delay: 9000,
+				navigation: {
+					arrows: {
+						enable: true
+					}
+				},
+				gridwidth: 1100,
+				gridheight: 500
+			});
+		});
+	</script>
+</body>
---
+++ public_html/malayalam1.php
@@ -0,0 +1,219 @@
+<?php ob_start(); ?>
+<?php session_start() ?>
+<?php 
+include("includes/usercredentials.php");
+?>
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Learn Malayalam Beginner - St. Stephen's Jacobite Syriac Orthodox Church, San Jose (San Francisco - Bay Area), California, USA</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta property="og:locale" content="es_ES" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Malayalam Class - Sessions" />
+<meta property="og:description" content="St.Stephen's Syriac Orthodox Church, San Jose (San Francisco - Bay Area), California, USA" />
+<meta property="og:url" content="https://www.ststephenssanjose.org/malayalam.php" />
+<meta property="og:image" content="https://www.ststephenssanjose.org/images/malayalam.png" />
+<meta property="og:site_name" content="Church" />
+<!-- Google Fonts -->
+<link href='https://fonts.googleapis.com/css?family=Noto+Serif:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+<!-- Styles -->
+<link href="font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css" />
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="css/owl-carousel.css" type="text/css" />
+<link rel="stylesheet" href="css/mediaelementplayer.min.css" />
+<link href="css/revolution.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="css/settings.css" media="screen" />
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+<link href="css/responsive.css" rel="stylesheet" type="text/css" />
+<link rel="alternate stylesheet" type="text/css" href="css/colors/red.css" title="color1" />
+<link rel="alternate stylesheet" type="text/css" href="css/colors/wedgewood.css" title="color2" />
+<link rel="stylesheet" type="text/css" href="css/colors/blue.css" title="color3" />
+<link rel="alternate stylesheet" type="text/css" href="css/colors/green.css" title="color4" />
+<link rel="alternate stylesheet" type="text/css" href="css/colors/darkgreen.css" title="color5" />
+<style>
+.member-detail {
+ position: relative;
+	}
+.logo img{
+	width:auto;
+}
+.title:before {
+    content: "\f005";
+}
+.image > img {
+    width: initial;
+}
+.sermon p {
+    margin-top: 5px;
+   font-size:14px;
+}
+
+.sermon h3 a {
+    color: #0dafa9;
+}
+ .sermon h4 {
+	 color: #b96969;
+}
+</style>
+</head>
+<body>
+<div class="theme-layout">
+<?php include "includes/pageheader.php";?>
+<div class="page-top">
+	<div class="parallax" style="background:url(images/malayalam_banner.png);"></div>	
+	<div class="container"> 
+		<h1><span>Malayalam Class - Beginner</span></h1>
+	</div>
+</div><!--- PAGE TOP -->
+<section>
+	<div class="block">
+		<div class="container">
+			<div class="row">
+                        <div id="session1" class="sermon">
+                            <div class="row">
+                                <div class="col-md-4">
+                                        <iframe width="370" height="241" src="https://www.youtube.com/embed/XE1xR54NKfc" 
+                                            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                                        </iframe>
+                                </div>
+                                <div class="col-md-6">
+                                    <h4> Session 1</h4>
+                                    <p>
+                                        Video explains how to write and pronounce the letters. 
+                                        The link below is the worksheet to print out OR replicate on a piece of binder paper for practice.
+                                    </p>
+                                    <h4><a href="/malayalam/beginner/malayalam_worksheet_1.pdf">Session 1 - Worksheet</a></h4>
+                                </div>
+                            </div>
+                        </div><!-- SERMON -->
+                        <div id="session2" class="sermon">
+                            <div class="row">
+                                <div class="col-md-4">
+                                        <iframe width="370" height="241" src="https://www.youtube.com/embed/TuQB0_71NK4" 
+                                            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                                        </iframe>
+                                </div>
+                                <div class="col-md-6">
+                                    <h4> Session 2</h4>
+                                    <p>
+                                        Video explains how to write and pronounce the letters. 
+                                        The link below is the worksheet to print out OR replicate on a piece of binder paper for practice.
+                                    </p>
+                                    <h4><a href="/malayalam/beginner/malayalam_worksheet_2.pdf">Session 2 - Worksheet</a></h4>
+                                </div>
+                            </div>
+                        </div><!-- SERMON -->
+                        <div id="session3" class="sermon">
+                            <div class="row">
+                                <div class="col-md-4">
+                                        <iframe width="370" height="241" src="https://www.youtube.com/embed/AB_u1qia_q4" 
+                                            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                                        </iframe>
+                                </div>
+                                <div class="col-md-6">
+                                    <h4> Session 3</h4>
+                                    <p>
+                                        Video explains how to write and pronounce the letters. 
+                                        The link below is the worksheet to print out OR replicate on a piece of binder paper for practice.
+                                    </p>
+                                    <h4><a href="/malayalam/beginner/malayalam_worksheet_3.pdf">Session 3 - Worksheet</a></h4>
+                                </div>
+                            </div>
+                        </div><!-- SERMON -->
+                        <div id="session3" class="sermon">
+                            <div class="row">
+                                <div class="col-md-4">
+                                        <iframe width="370" height="241" src="https://www.youtube.com/embed/MQBJAU6OpIY" 
+                                            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                                        </iframe>
+                                </div>
+                                <div class="col-md-6">
+                                    <h4> Session 3</h4>
+                                    <p>
+                                        Video explains how to write and pronounce the letters. 
+                                        The link below is the worksheet to print out OR replicate on a piece of binder paper for practice.
+                                    </p>
+                                    <h4><a href="/malayalam/beginner/malayalam_worksheet_4.pdf">Session 4 - Worksheet</a></h4>
+                                </div>
+                            </div>
+                        </div><!-- SERMON -->
+			</div>
+		</div>
+	</div>
+</section>	
+<?php include("includes/pagefooter.php");?>
+</div>
+	<!-- SCRIPTS-->
+	<script type="text/javascript" src="js/modernizr.custom.17475.js"></script>
+	<script src="js/jquery.1.10.2.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.poptrox.min.js" type="text/javascript"></script>
+	<script src="js/enscroll-0.5.2.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+	<script src="js/mediaelement-and-player.min.js"></script>
+    <script src="js/script.js"></script>
+    <script src="js/styleswitcher.js"></script>
+	<script src="js/jquery.isotope.min.js"></script>
+	<script src="js/jquery.minimalect.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="js/jquery.downCount.js"></script> 
+	<script>
+	$(window).load(function(){
+		$(function(){
+			var $portfolio = $('.mas-gallery');
+			$portfolio.isotope({
+			masonry: {
+			  columnWidth: 1
+			}
+			});
+		});
+	});
+	</script>
+	<script>
+    $(document).ready(function() {
+		$("#select1").minimalect({ theme: "bubble", placeholder: "Select Gender" });
+		$("#select2").minimalect({ theme: "bubble", placeholder: "Select Age" });
+		$("#select3").minimalect({ theme: "bubble", placeholder: "Select Your Area" });
+		$(".tweets-slides").owlCarousel({
+			autoPlay: 5000,
+			slideSpeed:1000,
+			singleItem : true,
+			transitionStyle : "fadeUp",		
+			navigation : false
+		}); /*** TWEETS CAROUSEL ***/
+		$(".team-carousel").owlCarousel({
+			autoPlay: 8000,
+			rewindSpeed : 3000,
+			slideSpeed:2000,
+			items : 4,
+			itemsDesktop : [1199,3],
+			itemsDesktopSmall : [979,2],
+			itemsTablet : [768,2],
+			itemsMobile : [479,1],
+			navigation : false,
+		}); /*** TEAM CAROUSEL ***/
+	});	
+	$('audio,video').mediaelementplayer();
+	</script>
+	<!-- SLIDER REVOLUTION -->
+    <script type="text/javascript" src="js/revolution/jquery.themepunch.tools.min.js"></script>   
+    <script type="text/javascript" src="js/revolution/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="js/revolution/extensions/revolution.extension.slideanims.min.js"></script>
+	<script type="text/javascript" src="js/revolution/extensions/revolution.extension.layeranimation.min.js"></script>
+	<script type="text/javascript" src="js/revolution/extensions/revolution.extension.navigation.min.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function() { 
+		   jQuery("#slider1").revolution({
+		      sliderType:"standard",
+		      sliderLayout:"fullwidth",
+		      delay:9000,
+		      navigation: {
+		          arrows:{enable:true} 
+		      }, 
+		      gridwidth:1100,
+		      gridheight:500
+		    }); 
+		}); 
+	</script>
+</body>
